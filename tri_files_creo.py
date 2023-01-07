64f7602b48ed66b7ec144aa9e5d9dde1e2a9f1f4
FoxtrotPapa_25 <Francois25@users.noreply.github.com>
2023-01-07 22:31:53 +0100

    Squashed commit of the following:
    commit 682fe597b254fd2348a6fc61a7f94b9b1f98f000
    Author: FoxtrotPapa_25 <Francois25@users.noreply.github.com>
    Date:   Sat Jan 7 21:53:57 2023 +0100
    
        bugg fixe - dossier incorrect choix autre Y et N
    
    commit e4bbd390f648ee386f6b51bd8e2034c3ece26854
    Author: FoxtrotPapa_25 <Francois25@users.noreply.github.com>
    Date:   Sat Jan 7 21:44:15 2023 +0100
    
        ajout fichier .exe
    
    commit e77fbd43f5afa1b595108949d367693612d91986
    Author: FoxtrotPapa_25 <Francois25@users.noreply.github.com>
    Date:   Sat Jan 7 21:39:41 2023 +0100
    
        bugg fixed - pas de suppression de fichiers
    
    commit 3ede4f8878c9bff0ab43a646fa358e3bf62a0224
    Author: FoxtrotPapa_25 <Francois25@users.noreply.github.com>
    Date:   Sat Jan 7 18:10:51 2023 +0100
    
        retrait du # os.remove
    
    commit cc20a322bf6d7d2e19b53d05c4a075a7f27bb514
    Author: FoxtrotPapa_25 <Francois25@users.noreply.github.com>
    Date:   Sat Jan 7 18:08:58 2023 +0100
    
        bugg fixed - traitement nouveau dossier
    
    commit fcd57b1193ce256826470895e35ffab3b5189666
    Author: FoxtrotPapa_25 <Francois25@users.noreply.github.com>
    Date:   Sat Jan 7 17:35:15 2023 +0100
    
        nouveau traitement après le premier

--- tri_files_creo.py
+++ tri_files_creo.py
@@ -3,6 +3,11 @@
 
 import os
 
+choose_folder = ''
+list_files = ''
+lasts_files = ''
+list_file_delete = ''
+
 def new_folder():
     try:
         choose_folder = input("Quel dossier traiter ? : ")
@@ -10,7 +15,6 @@ def new_folder():
         return choose_folder
 
     except:
-        #exit()
         choice = input("Dossier incorrect, voulez vous abandonner ?\n  Y - N : ")
         if choice.upper() == 'Y':
             exit()
@@ -18,9 +22,9 @@ def new_folder():
             new_folder()
         else:
             print("Vous devez entrer un choix entre Y et N")
-            exit()
+            start()
 
-def order_files(): 
+def order_files(list_files): 
     extension_max = '0'
     last_name_file = ''
     latest_file = ''
@@ -55,20 +59,17 @@ def order_files():
     list_latest_file.append(latest_file)
     return list_latest_file
 
-
-def file_to_delete():
+def file_to_delete(list_files, lasts_files):
     list_file_delete = []
     for file in list_files:
         if not file in lasts_files:
             list_file_delete.append(file)
     return list_file_delete
 
-
-def delete_file():
+def delete_file(list_file_delete, choose_folder, list_files, lasts_files):
     if len(list_file_delete) == 0:
-        print("\nAucuns fichiers à supprimer")
-        input("Pressez ENTER")
-        exit()
+        print("\nAucuns fichiers à supprimer\n")
+        return
     print(f"\nEtes vous sûr de vouloir supprimer le(s) {len(list_file_delete)} fichier(s) suivant(s) du dossier {choose_folder} ?")
     for i in list_file_delete:
         print(i)
@@ -83,19 +84,24 @@ def delete_file():
                 os.chdir(choose_folder)
                 os.remove(file)
         print(f"{len(list_file_delete)} fichiers supprimés, dossier traité.")
+        choose_folder = ''
+        return choose_folder
 
+def start():
+    choose_folder = new_folder()
+    os.chdir(choose_folder)
+    list_files = os.listdir()
+    lasts_files = order_files(list_files)
+    list_file_delete = file_to_delete(list_files, lasts_files)
+    delete_file(list_file_delete, choose_folder, list_files, lasts_files)
+    another_folder()
 
-choose_folder = new_folder()
-os.chdir(choose_folder)
-list_files = os.listdir()
-lasts_files = order_files()
-list_file_delete = file_to_delete()
-delete_file()
-print("\nFin : appuiez ENTER")
-input("")
+def another_folder():
+    choice = input("voulez vous traiter un autre dossier ?\n Y - N : ")
+    if choice.upper() == 'Y':
+        start()
+    else:
+        exit()
+ 
 
-# choice = input("voulez vous traiter un autre dossier ?\n Y - N : ")
-# if choice.upper() == 'Y':
-#     start()
-# else:
-#     exit()
+start()
